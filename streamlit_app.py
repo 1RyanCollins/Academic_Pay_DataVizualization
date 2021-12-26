788673427e7c10bc593feef1239179d06002b8d9
Ryan Collins <45883184+1RyanCollins@users.noreply.github.com>
2021-12-26 16:16:28 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
 import pandas as pd
+pip install git+https://github.com/adamcupial/lighthouse-python.git 
+from lighthouse import LighthouseRunner
+  
 
 st.write("SEO Project - Google Lighthouse Automation")
+
+report = LighthouseRunner('https://www.espn.com/', form_factor='desktop', quiet=False).report
+print(report)

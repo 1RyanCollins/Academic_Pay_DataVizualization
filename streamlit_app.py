b0153304fb7c95df5913ca20fcecc0f31dd59fe4
Ryan Collins <45883184+1RyanCollins@users.noreply.github.com>
2021-12-26 16:53:28 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ url = 'https://www.espn.com/'
 import urllib.request, json
 
 #Note that you can insert your URL with the parameter URL and you can also modify the device parameter if you would like to get the data for desktop.
-response = urllib.request.urlopen(url, key=API+Key)
+response = urllib.request.urlopen(url)
 data = json.loads(response.read())  
 
 overall_score = data["lighthouseResult"]["categories"]["performance"]["score"] * 100

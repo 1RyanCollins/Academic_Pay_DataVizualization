f11e85a2782a999884bff4ed917ec8c2c1476ed9
Ryan Collins <45883184+1RyanCollins@users.noreply.github.com>
2021-12-27 01:16:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import requests
 import pandas
 import GoogleSearch
-from googlesearch import GoogleSearch
-gs = GoogleSearch("An intriguing query")
-for url in gs.top_urls():
-  print(url)
+from googlesearch import search
+query = "Geeksforgeeks"
+ 
+for j in search(query, tld="co.in", num=10, stop=10, pause=2):
+    print(st.write(j))
 
 
 

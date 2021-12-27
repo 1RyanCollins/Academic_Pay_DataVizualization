2935f126275981aff45f8bc2ac31a0c390a696a1
Ryan Collins <45883184+1RyanCollins@users.noreply.github.com>
2021-12-27 00:58:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,6 @@ import requests
 import pandas
 
 
-try:
-    session = HTMLSession()
-    response = session.get(url)
-     
-except requests.exceptions.RequestException as e:
-    print(e)
-    
-title =  response.html.find('title')
-title = title[0].text
-
-
-
 st.write("SEO Project - Google Lighthouse Automation")
 st.write(title)
 

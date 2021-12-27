d2883dead0ef461b5bf2b97bc84c29782c25d4e5
Ryan Collins <45883184+1RyanCollins@users.noreply.github.com>
2021-12-27 00:50:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,12 @@
-#import libraries
-import streamlit as st
-import pandas as pd
-import json
-import csv
-from time import sleep
 import requests
-git clone https://github.com/wention/BeautifulSoup4.git
-from bs4 import BeautifulSoup
+import requests-HTML
+import install pandas
+import install regex
+import install urlparse4
 
-#get pagee
-page = requests.get("https://dataquestio.github.io/web-scraping-pages/simple.html")
 
-#print response code
-print(page)
 
-soup = BeautifulSoup(page.content, 'html.parser')
-
-#get title
-Title = soup.title
- 
 st.write("SEO Project - Google Lighthouse Automation")
-st.write(Title)
+
 
 

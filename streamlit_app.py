60b998a1caa7f7c6fdce1a06139fa3d36f5bc178
Ryan Collins <45883184+1RyanCollins@users.noreply.github.com>
2022-01-02 17:55:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,18 @@ import requests
 import pandas
 import streamlit as st
 from bs4 import BeautifulSoup
+from googlesearch import search
 
- 
+
+#run a google search 
+results = search("Google", num_results=10)
+
 url = "https://confluence.atlassian.com/bitbucketserver/basic-git-commands-776639767.html"
 
 req = requests.get(url)
 soup = BeautifulSoup(req.content, 'html.parser')
 st.title("SEO Keyword Web App")
-st.text(soup.prettify())
+st.text(results)
 
 
 

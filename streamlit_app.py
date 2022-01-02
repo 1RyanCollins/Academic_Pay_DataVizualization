f83cd6a7c6b99ef40f6ff4f1416c9f477d20f64f
Ryan Collins <45883184+1RyanCollins@users.noreply.github.com>
2022-01-02 17:05:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,29 @@
 import requests
 import pandas
+import streamlit as st
+st.title('Uber pickups in NYC')
 
-from googlesearch import search
-query = "Geeksforgeeks"
- 
-for j in search(query, tld="co.in", num=10, stop=10, pause=2):
-    print(st.write(j))
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html_doc)
+
+code = soup.prettify()
+st.text(code)
+
+
 
 
 

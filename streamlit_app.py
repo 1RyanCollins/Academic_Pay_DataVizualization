9ad4fa61750bcad92ba22b21ee8f256db9d0b4aa
Ryan Collins <45883184+1RyanCollins@users.noreply.github.com>
2022-01-02 17:52:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,13 @@ import streamlit as st
 from bs4 import BeautifulSoup
 
  
-URL = "http://www.values.com/inspirational-quotes"
-r = requests.get(URL)
-  
-soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
+URL = "https://confluence.atlassian.com/bitbucketserver/basic-git-commands-776639767.html"
+
+req = requests.get(URL, headers)
+soup = BeautifulSoup(req.content, 'html.parser')
 st.title("SEO Keyword Web App")
 st.text(soup.prettify())
+
 
 
 

d5350342494a4ca8381cd4ce1a75231ae6ff64c0
Ryan Collins <45883184+1RyanCollins@users.noreply.github.com>
2022-01-02 17:48:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,17 +3,13 @@ import pandas
 import streamlit as st
 from bs4 import BeautifulSoup
 
-# scraping a wikipedia article
-url_link = 'https://www.geeksforgeeks.org/how-to-scrape-all-pdf-files-in-a-website/'
-request = requests.get(url_link)
  
-Soup = BeautifulSoup(request.text, 'lxml')
- 
-# creating a list of all common heading tags
-heading_tags = ["h1", "h2", "h3"]
-for tags in Soup.find_all(heading_tags):
-    st.text((tags.name + ' -> ' + tags.text.strip()))
-    
+URL = "http://www.values.com/inspirational-quotes"
+r = requests.get(URL)
+  
+soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
+st.title(SEO Keyword Web App)
+st.text(soup.prettify())
 
 
 

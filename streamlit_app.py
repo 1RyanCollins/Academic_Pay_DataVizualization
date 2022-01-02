cb561072605a1cdde03a307e64012ef54ba97754
Ryan Collins <45883184+1RyanCollins@users.noreply.github.com>
2022-01-02 17:48:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ URL = "http://www.values.com/inspirational-quotes"
 r = requests.get(URL)
   
 soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
-st.title(SEO Keyword Web App)
+st.title("SEO Keyword Web App")
 st.text(soup.prettify())
 
 

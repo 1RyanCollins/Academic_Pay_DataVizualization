947ae998a783e581d7f3b9b6659fe52aa4e40e90
Ryan Collins, PhD <45883184+1RyanCollins@users.noreply.github.com>
2022-01-02 23:27:17 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,26 +4,30 @@ import streamlit as st
 from bs4 import BeautifulSoup
 from googlesearch import search
 
-url_input = st.text_input("Enter your keyword")
+#url_input = st.text_input("Enter your keyword")
 
 #run a google search 
 #keyword
 
 #top10 results
-results = []
+#results = []
 
 #####code here to pull search results#######
 
 
-for item in results: 
-    soup = BeautifulSoup(item, 'html.parser')
-    title = soup.find("title")
-    ##h1 tags 
+#for item in results: 
+url = "https://www.bbc.com/culture/article/20210902-the-power-of-the-dog-a-five-star-brooding-melodrama"
+soup = BeautifulSoup(html, 'html.parser')
+title = soup.find("title")
+
     
 ###determine position in results####
 
 st.title("SEO Keyword Web App")
 st.text(title)
+
+for heading in soup.find_all(["h1", "h2", "h3"]):
+    print(heading.name + ' ' + heading.text.strip())
 
 
 

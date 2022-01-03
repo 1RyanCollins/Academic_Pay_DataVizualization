f1d49d34d8f05831979f9bc58b6a3d0a76161fb3
Ryan Collins, PhD <45883184+1RyanCollins@users.noreply.github.com>
2022-01-02 18:23:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,11 +16,11 @@ for j in search(url_input, tld="co.in", num=10, stop=10, pause=2):
 
 
 for item in results: 
-    req = requests.get(item)
-    soup = BeautifulSoup(req.content, 'html.parser')
-    st.text(req)
+    soup = BeautifulSoup(item, 'html.parser')
+    title = soup.find("title")
 
 st.title("SEO Keyword Web App")
+st.text(title)
 
 
 

007418a39fa3e96c06b01e4cfd218885e00f8d09
Ryan Collins, PhD <45883184+1RyanCollins@users.noreply.github.com>
2022-01-02 18:19:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,15 @@ import streamlit as st
 from bs4 import BeautifulSoup
 from googlesearch import search
 
+url_input = st.text_input("Enter your keyword")
 
 #run a google search 
 #keyword
 
 #top10 results
-results = search("Google", num_results=10)
-
+results = []
+for j in search(url_input, tld="co.in", num=10, stop=10, pause=2):
+    results.append(j)
 
 #url
 url = "https://confluence.atlassian.com/bitbucketserver/basic-git-commands-776639767.html"

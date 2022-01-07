1e831628d7e99a6030728d3de4b9797dcab89a57
Ryan Collins, PhD <45883184+1RyanCollins@users.noreply.github.com>
2022-01-06 20:37:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,9 @@
-import requests
-import pandas
-import streamlit as st
-from bs4 import BeautifulSoup
-from googlesearch import search
+import pandas as pd
 
-#url_input = st.text_input("Enter your keyword")
+df = pd.read_csv("")
+st.write(df)  # visualize my dataframe in the Streamlit app
 
-#run a google search 
-#keyword
-
-#top10 results
-#results = []
-
-#####code here to pull search results#######
-
-
-#for item in results: 
-import requests
-page = requests.get("https://en.wikipedia.org/wiki/List_of_dog_breeds")
-soup = BeautifulSoup(page.content, 'html.parser')
-title = soup.title 
-title = title.contents
-
-number_of_words = len(title)
-position = number_of_words.index("dog")
-
-
-   
-
-st.title("SEO Keyword Web App")
+st.title("Adjunct & PhD Student Data Visualization Project")
 st.text(position)
 
 

939779ec9b114fc2f9ee6057fad197bcf0bc868d
Ryan Collins, PhD <45883184+1RyanCollins@users.noreply.github.com>
2022-01-06 20:42:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv("")
+df = pd.read_csv("Academic_Data.csv")
+st.title("Adjunct & PhD Student Data Visualization Project")
 st.write(df)  # visualize my dataframe in the Streamlit app
 
-st.title("Adjunct & PhD Student Data Visualization Project")
-st.text(position)
+
+
 
 
 
